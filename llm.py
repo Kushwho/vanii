632cb9daa8ded2d5bb1d1f36550ac66b029918c3
bhaveshAswani112 <aswanib133@gmail.com>
2024-08-31 23:55:35 +0530

    bug fixed

--- llm.py
+++ llm.py
@@ -143,7 +143,6 @@ def streaming(session_id,transcript):
             yield(chunk)
         # print(f"It took {time.time()-starttime} seconds for llm response")
         logging.info(f"It took {time.time()-starttime} seconds for llm response")
-        return response
     except Exception as e :
         logging.error(f"Error in generating response {e}")
         return "Sorry , there is some error"
@@ -152,10 +151,13 @@ def streaming2(session_id,transcript):
     try :
         starttime = time.time()
         config = {"configurable": {"session_id": session_id}}
+        response = ""
         for chunk in chain_with_history.stream({"question" : transcript},config=config) :
+            response += chunk
             yield(chunk)
         # print(f"It took {time.time()-starttime} seconds for llm response")
         logging.info(f"It took {time.time()-starttime} seconds for llm response")
+        return response
     except Exception as e :
         logging.error(f"Error in generating response {e}")
         return "Sorry , there is some error"
