624df99f066f2b944424af8e473feff9d997620e
bhaveshAswani112 <aswanib133@gmail.com>
2024-09-13 01:32:24 +0530

    cors fixed

--- app_socketio.py
+++ app_socketio.py
@@ -41,10 +41,13 @@ deepgram = deepgram_service.get_client()
 
 # Initialize Flask and SocketIO
 cors_allowed_origins = os.getenv("CORS")
+cors = '*'
+if cors_allowed_origins :
+    cors = cors_allowed_origins.split(',')
 app_socketio = Flask("app_socketio")
 app_socketio.config.from_object(Config)
 db.init_app(app_socketio)
-socketio = SocketIO(app_socketio, cors_allowed_origins=cors_allowed_origins)
+socketio = SocketIO(app_socketio, cors_allowed_origins=cors)
 
 # Initialize a dictionary to store Deepgram connections
 dg_connections = {}
