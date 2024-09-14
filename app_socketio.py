a95d92f33fb25f2f90a6957afd5eec76a26d2b20
bhaveshAswani112 <aswanib133@gmail.com>
2024-09-15 01:35:22 +0530

    email removed

--- app_socketio.py
+++ app_socketio.py
@@ -291,7 +291,6 @@ def handle_session_start(data):
 @socketio.on('join')
 def join(data):
     room_name = data['sessionId']
-    email = data['email']
     voice = data['voice']
     logging.info(f"Room has been created for sessionId {room_name}")
     join_room(room_name)
